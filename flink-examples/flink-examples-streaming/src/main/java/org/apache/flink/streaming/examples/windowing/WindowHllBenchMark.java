7c63d6d10bc6716c580e96544935808d17778eaa
hongyuzhou <hongyuzhou@didiglobal.com>
2022-09-07 17:14:07 +0800

    [flink-datasketch] fix WindowHllBenchMark

--- flink-examples/flink-examples-streaming/src/main/java/org/apache/flink/streaming/examples/windowing/WindowHllBenchMark.java
+++ flink-examples/flink-examples-streaming/src/main/java/org/apache/flink/streaming/examples/windowing/WindowHllBenchMark.java
@@ -20,9 +20,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public class HllBenchMark{
+public class WindowHllBenchMark{
 
-    private static final Logger LOG = LoggerFactory.getLogger(HllBenchMark.class);
+    private static final Logger LOG = LoggerFactory.getLogger(WindowHllBenchMark.class);
 
     public static void main(String[] args) throws Exception {
 
